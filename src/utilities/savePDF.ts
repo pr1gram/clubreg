6a56a63c1f8aea0c7acd2575d83b83aa4d269869
pr1gram <natchathornmoopimon@gmail.com>
2024-06-08 19:26:47 +0700

    update: update puppeteer

--- src/utilities/savePDF.ts
+++ src/utilities/savePDF.ts
@@ -12,7 +12,7 @@ export async function savePDF(url: string) {
           args: [],
           executablePath:
             process.platform === "win32"
-              ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+              ? "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
               : process.platform === "linux"
               ? "/usr/bin/google-chrome"
               : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
